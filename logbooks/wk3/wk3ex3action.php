9c4768f71655ea350ff27322ca46e4a851211b27
Ana Elderton <ana.c.elderton@gmail.com>
2024-02-25 15:33:26 +0000

    fixed code after seeing what break does

--- logbooks/wk3/wk3ex3action.php
+++ logbooks/wk3/wk3ex3action.php
@@ -2,9 +2,15 @@
   switch ($_POST["radcourse"])
   {
     case "HNC" : echo "You have selected an HNC course";
+        break;
     case "HND" : echo "You have selected an HND course";
+        break;
     case "BSC" : echo "You have selected a BSC course";
-    case "PrtBSC" : echo "You have selecter a Part Time BSC course";
+        break;
+    case "PrtBSC" : echo "You have selected a Part Time BSC course";
+        break;
     default    : echo "No course selected";
+        break;
   }
 
+//fixed the code so it works correctly after seeing what break does
