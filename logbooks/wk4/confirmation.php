1d06a4f001db311a6b1a2c74dc8f81de4dae696d
Ana Elderton <ana.c.elderton@gmail.com>
2024-02-25 18:27:20 +0000

    now completed session method

--- logbooks/wk4/confirmation.php
+++ logbooks/wk4/confirmation.php
@@ -1,4 +1,5 @@
 <?php
-   echo "<h2> Your order qty is $_COOKIE[selqty] </h2></br>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+    session_start();
+    echo "<h2> Your order qty is " . $_SESSION['qty'] . "</h2></br>";
+    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
 ?>
--- logbooks/wk4/selectcolour.php
+++ logbooks/wk4/selectcolour.php
@@ -1,12 +1,11 @@
 <html>
     <head><title>Select colour page</title></head>
-    <?php $cookieName = "selqty";
-    setcookie($cookieName, $_POST["selqty"]);
-    ?>
+    <?php session_start(); ?>
     <body>
             <form action="confirmation.php"  method="post">
 	            Select the colour for the <?php echo $_POST["selqty"] ?> widgets you are ordering
-   	                <select name="selcolour">
+   	            <?php $_SESSION['qty'] = $_POST["selqty"]; ?>
+                <select name="selcolour">
    	                    <option>white</option>
 	                    <option>red</option>
 	                    <option>yellow</option>
