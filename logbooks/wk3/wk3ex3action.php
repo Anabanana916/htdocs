8673b8e9e82c12b340ea597d3fa6c2734e4d3dcb
Ana Elderton <ana.c.elderton@gmail.com>
2024-02-25 15:22:59 +0000

    removing breaks from PHP to see what happens

--- logbooks/wk3/wk3ex3action.php
+++ logbooks/wk3/wk3ex3action.php
@@ -2,13 +2,9 @@
   switch ($_POST["radcourse"])
   {
     case "HNC" : echo "You have selected an HNC course";
-                 break;
     case "HND" : echo "You have selected an HND course";
-                 break;
     case "BSC" : echo "You have selected a BSC course";
-                 break;
     case "PrtBSC" : echo "You have selecter a Part Time BSC course";
-                break;
     default    : echo "No course selected";
   }
 
