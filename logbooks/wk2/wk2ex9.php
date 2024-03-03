3e5c6697e66e963bf13788e5cafa754cfc25a7bd
Ana Elderton <ana.c.elderton@gmail.com>
2024-03-03 22:53:54 +0000

    went back and completed wk2

--- logbooks/wk2/wk2ex9.php
+++ logbooks/wk2/wk2ex9.php
@@ -1,15 +1,11 @@
 <?php
-
-    $topmodules[0] = "Internet Systems Development";
-    $topmodules[5] = "Programming 1";
-    $topmodules[10] = "Programming 2";
-    $topmodules[30] = "OOAD";
-    $topmodules[40] = "Software Engineering";
-
-    $value = 40;
-    foreach($topmodules as $index)
+    //I found a style of creating arrays that works for me
+    $topmodules = array("Internet Systems Development" => 0,
+    "Programming 1" => 5, "Programming 2" => 10, "OOAD" => 30,
+    "Software Engineering" => 40) ;
+    
+    foreach($topmodules as $index => $value)
     {
         if($index == 40) break;
         echo "Index in $index and value is $value <br/>";
     }
-    //HELP
